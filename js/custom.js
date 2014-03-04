572d285e3f8b5084eb1e706e83414a85ed6724fa
noahlange <Noah@Noahs-MacBook-Pro.local>
2014-03-03 18:26:06 -0600

    Made some more changes.

--- js/custom.js
+++ js/custom.js
@@ -22,6 +22,55 @@ $(".ajax-link").click(function(e) {
 	resize();
 })
 
+var myPlaylist = [
+
+	{
+        mp3:'files/the-road.mp3',
+        title:'The Road',
+        artist:'Noah Lange',
+        buy:'http://bandcamp.com/noahlange',
+        price:'1',
+        duration:'2:42',
+        cover:'images/chicago.jpg'
+    },
+    {
+        mp3:'files/chicago-pt-i.mp3',
+        title:'Chicago, Pt. I',
+        artist:'Noah Lange',
+        buy:'http://bandcamp.com/noahlange',
+        price:'1',
+        duration:'3:46',
+        cover:'images/chicago.jpg'
+    },
+    {
+        mp3:'files/twenty-second-century-blues.mp3',
+        title:'22nd Century Blues',
+        artist:'Noah Lange',
+        buy:'http://bandcamp.com/noahlange',
+        price:'1',
+        duration:'3:12',
+        cover:'images/chicago.jpg'
+    },    
+    {
+        mp3:'files/rosie-holden.mp3',
+        title:'Rosie Holden',
+        artist:'Noah Lange',
+        buy:'http://bandcamp.com/noahlange',
+        price:'1',
+        duration:'6:12',
+        cover:'images/chicago.jpg'
+    },
+        {
+        mp3:'files/chicago-pt-ii.mp3',
+        title:'Chicago, Pt. II',
+        artist:'Noah Lange',
+        buy:'http://bandcamp.com/noahlange',
+        price:'1',
+        duration:'1:56',
+        cover:'images/chicago.jpg'
+    },
+];
+
 $(document).ready(function(){
 	var description = 'This extended play was written and recorded over the course of January and February 2014 at Luther College in Decorah, Iowa.';
 
